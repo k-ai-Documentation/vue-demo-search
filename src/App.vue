7bbbe46bec807fb20b5de845fac5ae428fa5703d
yanjingtui <meirunzhong@outlook.com>
2024-04-16 17:06:26 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template lang="pug">
-SearchPage
+.content
+    .logo 
+        img(:src="kaiLogo")
+    SearchBox
 </template>
 
 <script setup lang="ts">
-import SearchPage from './components/SearchPage.vue';
+import SearchBox from './components/SearchBox.vue';
 import 'kai-assets/styles/main.scss';
+import kaiLogo from "kai-assets/logo.svg"
+
 </script>
+
+<style lang="scss">
+.content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+
+    .logo {
+        width: 133px;
+        height: fit-content;
+        margin-top: 100px;
+        margin-bottom: 56px;
+        filter: var(--svg-filter-white-color);
+    }
+}
+</style>
 
